bf38653515c36a0691c38af8cac01001e90caf39
Alexander Cuji <edd_alex.66@hotmail.com>
2023-03-05 18:38:45 -0300

    Init test 1 signalR

---
+++ SignalRExample/wwwroot/js/usersCount.js
@@ -0,0 +1,36 @@
+﻿//Create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes aka receive  notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    console.log("Valor Hub: "+value.toString());
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    console.log("Valor Hub: " + value.toString());
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+function fullfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    //reject logs
+    console.log("Hubo un error al conectarse a UserHub ")
+
+}
+
+//start connectio
+connectionUserCount.start().then(fullfilled, rejected);
